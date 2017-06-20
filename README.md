c4740cde85cdf24dd35c375acf66350514311ff3
LobeTia <lobetia@gmail.com>
2017-06-20 10:12:29 +0200

    - Cleaned some code - Added README

---
+++ README.md
@@ -0,0 +1,30 @@
+# Fun with Genetic Algorithm
+
+An unnecessary, complicated way to learn some AI
+
+## Brief
+- Some carrots randomly spawn in an empty land
+- They can spread nearby other carrots or randomly spawn across the map
+- After a while, they rot and disappear
+- 10 rabbits with 10 different genomes, they see what there's in front of their and they can rotate left/right or go forward and reverse (like cars with long rabbit ears)
+- If a rabbit doesn't eat start losing life. When life is 0 they die
+- If a rabbit eat a carrot restore part of his life
+- When all the rabbits are dead I calculate the fitness of each one (life duration in frameCount) pick the top 4 rabbit, other 1 from the pool, brew 5 new rabbit configuration and restart
+- Game ends when a rabbit can live forever
+
+## Why
+- I (sadly) never worked with genetic algorithm 
+- I (sadly) never worked with neural network 
+- In general, I know very little about artificial intelligence
+[img]
+
+## ToDo
+- WebPack
+- EntityManager
+- Rabbit die when they reach 0 life
+- Carrot rottenizer 
+- RabbitManager, for creating and brewing rabbits
+- Rabbit N.Net
+- GeneticManager, for create a new Rabbit generation
+- Low Poly 3D! (because why not?)
+
--- background.js
+++ background.js
@@ -1,54 +1,84 @@
-function Plant() {
-    this.x = random(width)
-    this.y = random(height)
+function Plant(x = random(width), y = random(height)) {
+    this.x = x
+    this.y = y
 
     this.display = function () {
         noStroke()
         fill(52, 152, 219)
-        triangle(
-            relativePoint(0, this.x),
-            relativePoint(5, this.y),
-            relativePoint(20, this.x),
-            relativePoint(5, this.y),
-            relativePoint(10, this.x),
-            relativePoint(20, this.y)
-        )
-        triangle(
-            relativePoint(0, this.x),
-            relativePoint(15, this.y),
-            relativePoint(20, this.x),
-            relativePoint(15, this.y),
-            relativePoint(10, this.x),
-            relativePoint(0, this.y)
-        )
+        triangle(rP(0, this.x), rP(5, this.y), rP(20, this.x), rP(5, this.y), rP(10, this.x), rP(20, this.y))
+        triangle(rP(0, this.x), rP(15, this.y), rP(20, this.x), rP(15, this.y), rP(10, this.x), rP(0, this.y))
 
         fill(241, 196, 15)
+        ellipse(rP(10, this.x), rP(10, this.y), 8, 8)
+    }
+    this.update = function () {
+    }
+}
+
+function Rabbit(x = random(width), y = random(height)) {
+    this.x = x
+    this.y = y
+    this.life = 1000
+
+    this.lifePercentual = function () {
+        return this.life * 100 / 1000 / 100 * 40
+    }
+
+    this.display = function () {
+        // Draw rabbit
         noStroke()
-        ellipse(
-            relativePoint(10, this.x),
-            relativePoint(10, this.y),
-            8, 8
-        )
+        fill(149, 165, 166)
+        rect(rP(0, this.x), rP(0, this.y), 25, 15)
+        stroke(color(0, 0, 0, 30))
+        rect(rP(-5, this.x), rP(-15, this.y), 3, 15)
+        rect(rP(0, this.x), rP(-15, this.y), 3, 15)
+        quad(rP(-5, this.x), rP(0, this.y), rP(0, this.x), rP(-8, this.y), rP(5, this.x), rP(0, this.y), rP(0, this.x), rP(8, this.y))
+
+        // Draw life
+        if (this.life > 0) {
+            fill(255, 255, 255)
+            rect(rP(-10, this.x), rP(-24, this.y), 40, 5)
+            fill(0, 255, 0)
+            rect(rP(-10, this.x), rP(-24, this.y), this.lifePercentual(), 5)
+        } else {
+            fill(255, 0, 0)
+            rect(rP(-10, this.x), rP(-24, this.y), 40, 5)
+        }
+    }
+    this.update = function () {
+        if (this.life > 0) this.life--
     }
 }
 
 var entities = []
 var counter
-
+var canvas
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight)
+    canvas = createCanvas(window.innerWidth, window.innerHeight)
     entities.push(new Plant())
+    entities.push(new Rabbit(width / 2, height / 2))
     counter = 0
 }
 function draw() {
     background(46, 204, 113)
+
+    var textPlaceholder = `Fun with Genetic Algorithm
+Entities: ${entities.length}`
+
+    fill(0, 0, 0);
+    textSize(15)
+    textFont("monospace")
+    text(textPlaceholder, 5, 20, 300)
+
     if (++counter === 120) {
         entities.push(new Plant())
         counter = 0
     }
+
+    entities.map((entity) => entity.update())
     entities.map((entity) => entity.display())
 }
 
-function relativePoint(origin, delta) {
+function rP(origin, delta) {
     return origin + delta
 }
